bd8b04e788a16b6a78ea6e28fe92fc415d444515
Xiaoming Li <>
2024-12-18 19:18:29 +0800

    配置自定义 axios 客户端，实现拦截器解析请求内容

---
+++ src/plugins/myAxios.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const instance = axios.create(
+    {
+        baseURL: "http://localhost:8101/api",
+        timeout: 10000,
+        headers: {}
+    }
+)
+
+// 使用响应拦截器取出响应值
+instance.interceptors.response.use(
+    response => {
+        const data = response.data
+        if (data.code === 0) {
+            alert(1)
+            return data.data
+        }
+        console.error("request error", data)
+        return response.data
+    },
+    error => {
+        return Promise.reject(error)
+    }
+)
+
+export default instance;
