e7d97aa82688a0eecc3cda4e6552e7f999798653
Xiaoming Li <>
2024-12-22 23:25:14 +0800

    开发图片列表组件

--- src/components/PictureList.vue
+++ src/components/PictureList.vue
@@ -1,9 +1,56 @@
 <template>
-    图片列表
+    <!-- 图片列表  -->
+    <a-list :grid="{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 3, xxl: 3 }" :data-source="pictureList">
+        <template #renderItem="{ item }">
+            <a-list-item key="item.id">
+                <a-list-item-meta>
+                    <!-- <template #title>
+                        <a :href="item.href">{{ item.title }}</a>
+                    </template>
+<template #avatar><a-avatar :src="item.avatar" /></template> -->
+                </a-list-item-meta>
+                <!-- <a-card hoverable style="width: 240px;"> -->
+                <a-card hoverable>
+                    <template #cover>
+                        <!-- <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" /> -->
+                        <img alt="example" :src="item.url" />
+                    </template>
+                    <!-- <a-card-meta title="Europe Street beat">
+                    <template #description>www.instagram.com</template>
+                </a-card-meta> -->
+                    <a-card-meta :title="item.title">
+                        <template #description>{{ item.description }}</template>
+                    </a-card-meta>
+                </a-card>
+            </a-list-item>
+        </template>
+    </a-list>
 </template>
 
 <script setup lang="ts">
-    import { ref } from "vue";
+import { ref } from "vue";
+import { withDefaults, defineProps } from "vue";
+
+interface Props {
+    pictureList: any[]
+}
+
+const props = withDefaults(defineProps<Props>(), {  // 填充默认值
+    pictureList: () => []
+});
+
+const pagination = {
+    onChange: (page: number) => {
+        console.log(page);
+    },
+    pageSize: 3,
+};
+
 </script>
 
-<style scoped></style>
+<style scoped>
+    /* a-list-item a-card {
+        width: 100%;
+        height: 100%;
+    } */
+</style>
