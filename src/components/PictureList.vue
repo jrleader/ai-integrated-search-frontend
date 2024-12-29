36cbb29faa7382ecc765200d9b01b6176f88799d
Xiaoming Li <>
2024-12-29 11:28:09 +0800

    图片列表支持显示原图

--- src/components/PictureList.vue
+++ src/components/PictureList.vue
@@ -14,7 +14,7 @@
                 <a-card hoverable>
                     <template #cover>
                         <!-- <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" /> -->
-                        <img alt="example" :src="item.url" />
+                        <a :href="item.url"><img alt="example" :src="item.url" /></a>
                     </template>
                     <!-- <a-card-meta title="Europe Street beat">
                     <template #description>www.instagram.com</template>
