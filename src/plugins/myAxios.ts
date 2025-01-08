8fa0e0ed7945460d6444decc2f9f29f6f4cf5dfb
Xiaoming Li <>
2025-01-08 22:50:21 +0800

    更新 axios 客户端配置，增加动态环境配置

--- src/plugins/myAxios.ts
+++ src/plugins/myAxios.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
 
+export const isDev = process.env.NODE_ENV === 'development'
+
+const baseURLs = {
+    dev: "http://192.168.3.21:8101/api",
+    prod: "https://ai-integrated-search-134551-9-1334709079.sh.run.tcloudbase.com/api"
+}
+
 const instance = axios.create(
     {
-        baseURL: "http://localhost:8101/api",
+        // baseURL: "http://localhost:8101/api",
+        baseURL: isDev ? baseURLs.dev : baseURLs.prod,
+        // baseURL: process.env.VUE_APP_API_URL,
         timeout: 10000,
         headers: {}
     }
